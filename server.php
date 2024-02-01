afc8002ecaed7ec193372a1144ef5bb8ece4a076
Devix11 <davideagostini05@gmail.com>
2024-02-01 09:52:04 +0100

    asd

--- server.php
+++ server.php
@@ -1,20 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: http://vibes-internal.mrfreex.net:3339');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $postData = file_get_contents('php://input');
-    $requestData = json_decode($postData, true);
-
-    if (empty($requestData)) {
-        http_response_code(400);
-        echo json_encode(['success' => false, 'error' => 'Dati del modulo mancanti o non validi']);
-        exit;
-    }
-
-    $songTitle = $requestData['songTitle'];
-    $artist = $requestData['artist'];
+    $songTitle = $_POST['songTitle'];
+    $artist = $_POST['artist'];
 
     // Connessione al database MySQL
     $dbConnection = new mysqli('localhost', 'phpmyadmin', 'ciaone11', 'canzoni');
@@ -28,15 +15,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->bind_param('ss', $songTitle, $artist);
 
     if ($stmt->execute()) {
-        echo json_encode(['success' => true, 'message' => 'Richiesta aggiunta con successo']);
+        echo 'Richiesta aggiunta con successo';
     } else {
-        echo json_encode(['success' => false, 'error' => 'Errore nell\'inserimento della richiesta']);
+        echo 'Errore nell\'inserimento della richiesta';
     }
 
     $stmt->close();
     $dbConnection->close();
-} else {
-    http_response_code(405);
-    echo json_encode(['success' => false, 'error' => 'Metodo non consentito']);
 }
 ?>
