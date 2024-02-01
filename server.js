f3c0f4cb6fcb44b18ff9fee0e4f3a342131f20d3
Devix11 <davideagostini05@gmail.com>
2024-02-01 09:27:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3369; // Sostituisci con la porta che desideri utilizzare
 const mysql = require('mysql2'); // Assicurati di aver installato il modulo mysql2
 
-app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://vibes-internal.mrfreex.net:3339');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 // Configura la connessione al database MySQL
 const dbConnection = mysql.createConnection({
