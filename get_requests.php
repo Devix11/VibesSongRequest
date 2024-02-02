ba3d00d3415014a597a6d7836acb0e1f38daf73d
Devix11 <davideagostini05@gmail.com>
2024-02-02 09:47:20 +0100

    Update get_requests.php

--- get_requests.php
+++ get_requests.php
@@ -20,7 +20,7 @@ $result = $dbConnection->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $likes = $row['likes']; // Aggiungi questa riga per ottenere il numero di likes
-        echo '<li>' . $row['titolo'] . ' - ' . $row['artista'] .'</li>';
+        echo '<li><a style="color:black">' . $row['titolo'] . ' - ' . $row['artista'] .'</a></li>';
     }
 } else {
     echo '<p>Nessuna richiesta trovata.</p>';
