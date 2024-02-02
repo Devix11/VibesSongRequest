6edb4ccfffe3b50becbb8b93ce73b31e20fbd260
Devix11 <davideagostini05@gmail.com>
2024-02-02 08:49:20 +0100

    Update server.php

--- server.php
+++ server.php
@@ -14,15 +14,12 @@ if (isset($_POST['submit'])) {
     $sql = 'INSERT INTO richieste (titolo, artista) VALUES (?, ?)';
     $stmt = $dbConnection->prepare($sql);
     $stmt->bind_param('ss', $songTitle, $artist);
-} else {
-    // L'utente non ha cliccato sul link Instagram, non puoi processare la richiesta
-    echo 'Per favore, clicca sul link Instagram per seguire Vibes Padova.';
-}   
-
-        header('Location: requests.php');
-        exit;
-
+    
+    $stmt->execute();
     $stmt->close();
     $dbConnection->close();
+
+    header('Location: requests.php');
+    exit;
 }
 ?>
