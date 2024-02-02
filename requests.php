e7a57d478e16c7b1bbae7734c3bd30451ee1437c
Devix11 <davideagostini05@gmail.com>
2024-02-02 09:16:00 +0100

    Update requests.php

--- requests.php
+++ requests.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista Richieste</title>
-    <!-- Aggiungi il collegamento a Bootstrap CSS -->
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Collegamento a Tailwind CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-red-500 text-white">
     <header class="bg-primary text-white text-center py-4">
-        <h1>Lista Richieste</h1>
+        <h1 class="text-3xl">Lista Richieste</h1>
     </header>
-    <main class="container mt-4">
-        <ul class="list-group">
-            <li><a>Titolo - Artista</a></li>
-        </ul>
+    <main class="container mx-auto mt-4">
         <ul class="list-group">
             <?php include('get_requests.php'); ?>
         </ul>
@@ -23,10 +19,5 @@
     <footer class="bg-light text-center py-3">
         <p>&copy; 2024 Vibes Events</p>
     </footer>
-
-    <!-- Aggiungi il collegamento a Bootstrap JavaScript e jQuery -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
